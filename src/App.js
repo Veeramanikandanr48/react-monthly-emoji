e0a573e560595a020793116d01bf7ff621b84c1c
Veeramanikandanr48 <veeramanikandanr48@gmail.com>
2024-02-18 12:56:15 +0000

    inital

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import MonthlyEmojis from './components/MonthlyEmojis'
+import './App.css'
+
+// Replace your code here
+const App = () => <MonthlyEmojis />
+
+export default App
+
+// Domain Url https://monthly-emojis-in-react-idp-ccbp-tech.vercel.app/
